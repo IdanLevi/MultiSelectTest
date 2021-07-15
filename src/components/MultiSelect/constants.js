56bc45cf0578aa4bccec00b7e181c32d4b0b6243
IdanLevi <idanlevi6@gmail.com>
2021-07-15 18:36:11 +0300

    Delete src directory

--- src/components/MultiSelect/constants.js
+++
@@ -1,6 +0,0 @@
-export const ArrowDown = 'ArrowDown';
-export const ArrowUp = 'ArrowUp';
-export const Enter = 'Enter';
-export const BackSpace = 'Backspace';
-export const OptionClassName = 'Option';
-export const InnerInputClassName = 'InnerInput';
