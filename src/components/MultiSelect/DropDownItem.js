2be692d2994a68624677b2d98a46b73f53dfe75f
IdanLevi <idanlevi6@gmail.com>
2021-07-15 21:49:09 +0300

    Update DropDownItem.js

--- src/components/MultiSelect/DropDownItem.js
+++ src/components/MultiSelect/DropDownItem.js
@@ -6,8 +6,6 @@ export default function DropDownItem({ selectedOptions, option, toggleSelection,
   
   const myRef = useRef();
 
-  const vImageClassName = `arrow-static ${selectedOptions.find(so => so.value === option.value) ? '' : 'Hidden'}`;
-
   useEffect(() => {
     if (focusIndex === index) {
       myRef.current.focus();
@@ -17,7 +15,9 @@ export default function DropDownItem({ selectedOptions, option, toggleSelection,
   return (
     <div className="Option" index={index} onClick={() => toggleSelection(option)} ref={myRef} tabIndex="-1">
       <div className="optionSelectedIndicator">
-        <img src={vImage} className={vImageClassName} alt="Selected Indicator" />
+        {selectedOptions.find(so => so.value === option.value) &&
+          <img src={vImage} className="arrow-static" alt="Selected Indicator" />
+        }
       </div>
       <div className="optionLabel" title={option.label}>
         {option.label}
